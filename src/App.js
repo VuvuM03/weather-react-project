629709e030d27b82357506a61bc69e09b6a9c41d
VuvuM03 <139034985+VuvuM03@users.noreply.github.com>
2023-11-23 20:59:07 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather React App</h1>
-      <footer>
-        This project was coded by <a href="/">VuvuM</a> and is{" "}
-        <a
-          href="https://github.com/VuvuM03/weather-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather React App</h1>
+        <footer>
+          This project was coded by <a href="/">VuvuM</a> and is{" "}
+          <a
+            href="https://github.com/VuvuM03/weather-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
