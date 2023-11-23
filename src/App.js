14261ea13d512ff480067b0318f24db6c48e599d
VuvuM03 <139034985+VuvuM03@users.noreply.github.com>
2023-11-23 20:38:27 +0200

    Added Github Repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather React App</h1>
+      <footer>
+        This project was coded by <a href="/">VuvuM</a> and is{" "}
+        <a
+          href="https://github.com/VuvuM03/weather-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
